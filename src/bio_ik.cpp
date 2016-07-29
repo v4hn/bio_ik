d7f3cdc3b8b5c34c895ed88d3162156fde429034
sebastianstarke <starke@informatik.uni-hamburg.de>
2016-07-29 17:20:49 +0200

    added solution filtering

--- src/bio_ik.cpp
+++ src/bio_ik.cpp
@@ -105,7 +105,7 @@ namespace bio_ik {
                       double search_discretization);
 
       double IK_FitnessFunction(double* &input, int dimensionality, const geometry_msgs::Pose&) const;
-
+      double IK_BalancedFitnessFunction(const vector<double>& input, int dimensionality, const geometry_msgs::Pose&) const;
     private:
 
   };
@@ -148,6 +148,31 @@ namespace bio_ik {
     double random = gen(rng);
 
     return random*dP + (1.0-random)*dR;
+  }
+
+  double BIO_IK::IK_BalancedFitnessFunction(const vector<double>& input, int dimensionality, const geometry_msgs::Pose& target) const {
+    vector<geometry_msgs::Pose> poses;
+    getPositionFK(EndEffector, input, poses);
+    
+    //Position Error
+    double diffX = poses[0].position.x - target.position.x;
+    double diffY = poses[0].position.y - target.position.y;
+    double diffZ = poses[0].position.z - target.position.z;
+    double dP = sqrt(diffX*diffX + diffY*diffY + diffZ*diffZ);
+    
+    //Orientation Error
+    double dR = GetAngleDifference(
+      poses[0].orientation.x,
+      poses[0].orientation.y,
+      poses[0].orientation.z,
+      poses[0].orientation.w,
+      target.orientation.x,
+      target.orientation.y,
+      target.orientation.z,
+      target.orientation.w);
+
+    //Multi-Objective Weight Randomization
+    return 0.5*(dP+dR);
   }
 
   bool BIO_IK::initialize(const string &robot_description,
@@ -358,6 +383,7 @@ namespace bio_ik {
                                 moveit_msgs::MoveItErrorCodes &error_code,
                                 const vector<double> &consistency_limits,
                                 const kinematics::KinematicsQueryOptions &options) const {
+    const clock_t begin_time = clock();
 
     ROS_DEBUG_STREAM_NAMED("bio_ik","getPositionIK");
 
@@ -374,10 +400,10 @@ namespace bio_ik {
       dimensions[i].Min = limitsMin(i);
       dimensions[i].Max = limitsMax(i);
     }
-
     Evolution evolution(size, elites, dimensionality, dimensions, boost::bind(&BIO_IK::IK_FitnessFunction, this, _1, _2, ik_pose), ik_seed_state);
 
-    const clock_t begin_time = clock();
+    double seedFitness = IK_BalancedFitnessFunction(ik_seed_state, dimensionality, ik_pose);
+
     double accuracy = 0.001;
     int generations = 0;
     while((double)(clock() - begin_time) / CLOCKS_PER_SEC < 0.02) {
@@ -388,7 +414,15 @@ namespace bio_ik {
       solution[i] = evolution.GetPrototype().Genes[i];
     }
 
-    cout << "Generations: " << generations << " (" << ((double)(clock() - begin_time) / CLOCKS_PER_SEC) << "s)" << endl;
+    double solutionFitness = IK_BalancedFitnessFunction(solution, dimensionality, ik_pose);
+
+    if(seedFitness <= solutionFitness) {
+      solution = ik_seed_state;
+      cout << "Accuracy: " << seedFitness << endl;
+    } else {
+      cout << "Accuracy: " << solutionFitness << endl;
+    }
+    //cout << "Generations: " << generations << " (" << ((double)(clock() - begin_time) / CLOCKS_PER_SEC) << "s)" << endl;
     return true;
   }
 
